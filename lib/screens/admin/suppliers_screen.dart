ebf76c078a023b9cb7b035de4e68717f01a6f327
TheDanilore <arteagagordillol@gmail.com>
2026-06-14 00:26:43 -0500

    Se corrige/agrega PurchaseOrderFormScreen, SuppliersScreen

--- lib/screens/admin/suppliers_screen.dart
+++ lib/screens/admin/suppliers_screen.dart
@@ -249,7 +249,7 @@ class _SuppliersScreenState extends State<SuppliersScreen> {
                                   ),
                                   itemCount: pageItems.length,
                                   separatorBuilder:
-                                      (_, __) => const SizedBox(height: 12),
+                                      (_, _) => const SizedBox(height: 12),
                                   itemBuilder: (context, index) {
                                     final supplier = pageItems[index];
                                     return _SupplierCard(
