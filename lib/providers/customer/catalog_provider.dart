433aff717b733867b790dbd4d2c5aab8847f1982
TheDanilore <arteagagordillol@gmail.com>
2026-06-28 00:05:08 -0500

    Correccione paginacion CustomerCatalogScreen

--- lib/providers/customer/catalog_provider.dart
+++ lib/providers/customer/catalog_provider.dart
@@ -6,7 +6,7 @@ import 'package:inventory_store_app/services/customer/catalog_service.dart';
 
 class CustomerCatalogProvider extends ChangeNotifier {
   final CatalogService _service = CatalogService();
-  static const int pageSize = 4;
+  static const int pageSize = 24;
 
   // Search History
   List<String> _searchHistory = [];
